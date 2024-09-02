322c968216d8d372f885b2a801edfce8ab94d011
георгий савинцев <igsgo4645@gmail.com>
2024-09-02 18:17:02 +0400

    add user model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,37 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(AbstractUser):
-    pass
 
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+    phone_number = models.CharField(
+        max_length=35,
+        blank=True,
+        null=True,
+        verbose_name="Номер телефона",
+        help_text="Укажите номер телефона",
+    )
+    city = models.CharField(
+        max_length=35,
+        blank=True,
+        null=True,
+        verbose_name="Город",
+        help_text="Укажите название города",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        blank=True,
+        null=True,
+        verbose_name="Аватар",
+        help_text="Загрузите ваш аватар",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
