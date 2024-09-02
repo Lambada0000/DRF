6e50368811883fdcd578919f84b6afa6c1709090
георгий савинцев <igsgo4645@gmail.com>
2024-09-02 18:15:51 +0400

    add urls

---
+++ Ims/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from Ims.views import CourseViewSet, LessonCreateAPIView, LessonRetrieveAPIView, LessonListAPIView, LessonUpdateAPIView, LessonDestroyAPIView
+from Ims.apps import ImsConfig
+
+app_name = ImsConfig.name
+
+router = SimpleRouter()
+router.register("", CourseViewSet)
+
+urlpatterns = [
+    path("lessons/", LessonListAPIView.as_view(), name="lessons_list"),
+    path("lesson/<int:pk>", LessonRetrieveAPIView.as_view(), name="lesson_retrieve"),
+    path("lesson/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lessons_update"),
+    path("lesson/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lessons_delete"),
+    path("lesson/create/", LessonCreateAPIView.as_view(), name="lessons_create"),
+]
+
+urlpatterns += router.urls
