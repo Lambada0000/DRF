5c6cf61857efdba534dbd560eae08865b1743c29
георгий савинцев <igsgo4645@gmail.com>
2024-09-08 00:58:42 +0400

    add new model - Payments

--- users/models.py
+++ users/models.py
@@ -1,5 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.timezone import now
+
+from Ims.models import Course, Lesson
+
+payment_choices = (
+    ("Наличные", "Наличные"),
+    ("Перевод на счет", "Перевод на счет"),
+)
 
 
 class User(AbstractUser):
@@ -35,3 +43,59 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+
+class Payments(models.Model):
+    user = models.ForeignKey(
+        User,
+        verbose_name="пользователь",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    payment_date = models.DateTimeField(
+        default=now, verbose_name="Дата оплаты", blank=True, null=True
+    )
+    course = models.ForeignKey(
+        Course,
+        verbose_name="Курс",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    lesson = models.ForeignKey(
+        Lesson,
+        verbose_name="Урок",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    amount = models.PositiveIntegerField(verbose_name="Стоимость курса", help_text="Укажите стоимость курса"
+    )
+    payment_type = models.CharField(
+        max_length=50,
+        verbose_name="Способ оплаты",
+        default="Перевод на счет",
+        choices=payment_choices,
+    )
+    session_id = models.CharField(
+        max_length=250,
+        verbose_name="ID сессии",
+        blank=True,
+        null=True,
+        help_text="Укажите ID сессии",
+    )
+    payment_link = models.URLField(
+        max_length=400,
+        blank=True,
+        null=True,
+        verbose_name="Ссылка на оплату",
+        help_text="Укажите ссылку на оплату",
+    )
+
+    class Meta:
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
+
+    def __str__(self):
+        return self.amount
