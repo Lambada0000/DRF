b6c87ab87c0ec60b6b96b50e92f13a76b2a1592b
георгий савинцев <igsgo4645@gmail.com>
2024-09-08 00:58:15 +0400

    add method get_lessons for Course

--- Ims/serializers.py
+++ Ims/serializers.py
@@ -17,9 +17,13 @@ class LessonSerializer(ModelSerializer):
 
 class CourseDetailSerializer(ModelSerializer):
     lesson_count_in_course = SerializerMethodField()
+    lessons = SerializerMethodField()
 
     def get_lesson_count_in_course(self, obj):
         return obj.lesson_set.count()
+
+    def get_lessons(self, course):
+        return [lesson.description for lesson in Lesson.objects.filter(course=course)]
 
     class Meta:
         model = Course
