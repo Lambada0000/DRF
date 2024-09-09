2d32f6d3e1536552463d07a8b839034f00373e05
георгий савинцев <igsgo4645@gmail.com>
2024-09-09 21:57:15 +0400

    fix

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,23 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from Ims.apps import ImsConfig
-from users.views import UserListAPIView, UserRetrieveAPIView, UserUpdateAPIView, UserDestroyAPIView, UserCreateAPIView
+from users.views import (
+    UserListAPIView,
+    UserRetrieveAPIView,
+    UserUpdateAPIView,
+    UserDestroyAPIView,
+    UserCreateAPIView,
+)
 
 app_name = ImsConfig.name
 
 urlpatterns = [
-    path("lesson/", UserListAPIView.as_view(), name="user_list"),
-    path("lesson/<int:pk>/", UserRetrieveAPIView.as_view(), name="user_retrieve"),
-    path("lesson/<int:pk>/update/", UserUpdateAPIView.as_view(), name="user_update"),
-    path("lesson/<int:pk>/delete/", UserDestroyAPIView.as_view(), name="user_delete"),
-    path("lesson/create/", UserCreateAPIView.as_view(), name="user_create"),
+    path("user/", UserListAPIView.as_view(), name="user_list"),
+    path("user/<int:pk>/", UserRetrieveAPIView.as_view(), name="user_retrieve"),
+    path("user/<int:pk>/update/", UserUpdateAPIView.as_view(), name="user_update"),
+    path("user/<int:pk>/delete/", UserDestroyAPIView.as_view(), name="user_delete"),
+    path("user/create/", UserCreateAPIView.as_view(), name="user_create"),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
