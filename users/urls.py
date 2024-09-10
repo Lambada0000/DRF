cd8ba68ed9962c68baad69dfd55a69b6e4a05a74
георгий савинцев <igsgo4645@gmail.com>
2024-09-10 16:19:19 +0400

    add controllers in PaymentsViewSet

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
 from users.apps import UsersConfig
 from users.views import (
     UserListAPIView,
@@ -8,9 +8,13 @@ from users.views import (
     UserUpdateAPIView,
     UserDestroyAPIView,
     UserCreateAPIView,
+    PaymentsViewSet,
 )
 
 app_name = UsersConfig.name
+
+router = SimpleRouter()
+router.register(r"payment", PaymentsViewSet, basename="payment")
 
 urlpatterns = [
     path("", UserListAPIView.as_view(), name="user_list"),
@@ -21,3 +25,5 @@ urlpatterns = [
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+urlpatterns += router.urls
