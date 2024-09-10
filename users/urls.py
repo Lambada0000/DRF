4530584f94983748c7ff7eda053f59cae661a38f
георгий савинцев <igsgo4645@gmail.com>
2024-09-10 18:08:42 +0400

    fix

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from users.apps import UsersConfig
@@ -21,9 +22,9 @@ urlpatterns = [
     path("user/<int:pk>/", UserRetrieveAPIView.as_view(), name="user_retrieve"),
     path("user/<int:pk>/update/", UserUpdateAPIView.as_view(), name="user_update"),
     path("user/<int:pk>/delete/", UserDestroyAPIView.as_view(), name="user_delete"),
-    path("user/create/", UserCreateAPIView.as_view(), name="user_create"),
-    path("login/", TokenObtainPairView.as_view(), name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("register/", UserCreateAPIView.as_view(permission_classes=(AllowAny,)), name="register"),
+    path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
 ]
 
 urlpatterns += router.urls
